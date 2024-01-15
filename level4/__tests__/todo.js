fedff63fc29a242c53a96b1fc8fd3e4122e4d4b2
Gnaneswar Yalla <96044466+Gnanigit@users.noreply.github.com>
2024-01-15 19:37:39 +0530

    Update todo.js

--- level4/__tests__/todo.js
+++ level4/__tests__/todo.js
@@ -23,3 +23,51 @@ describe("TodoList Test Suite",()=>{
         expect(all[0].completed).toBe(true);
     })
 })
+
+describe("TodoList retrieval Test Suite", () => {
+  beforeAll(() => {
+    add({
+      title: "Finish Project",
+      completed: false,
+      dueDate: new Date("11-08-23"),
+    });
+    add({
+      title: "Complete Assignment",
+      completed: true,
+      dueDate: new Date("11-08-23"),
+    });
+    add({
+      title: "Test 21",
+      completed: false,
+      dueDate: new Date(),
+    });
+    add({
+      title: "Test 22",
+      completed: true,
+      dueDate: new Date(),
+    });
+    add({
+      title: "Test 31",
+      completed: false,
+      dueDate: new Date("11-08-24"),
+    });
+    add({
+      title: "Test 32",
+      completed: true,
+      dueDate: new Date("11-08-24"),
+    });
+  });
+  test("Test retrieval of overdue items", () => {
+    const length = overdue().length;
+    console.log(overdue());
+    expect(length).toBe(2);
+  });
+  test("Test retrieval of due today items", () => {
+    console.log(dueToday());
+    expect(dueToday().length).toBe(1);
+  });
+  test("Test retrieval of due later items", () => {
+    console.log(dueLater());
+    expect(dueLater().length).toBe(1);
+  });
+});
